bd52762a8bd93a327a9a842c23608a606813955f
GutsySpider <bowman.p.michael@gmail.com>
2026-01-12 10:45:09 -0800

    made hamburger button name more accessable.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,5 @@
 // Store the selected elements that we are going to use.
-const navbutton = document.querySelector('#ham-btn');
+const navbutton = document.querySelector('#hamburger');
 const navBar = document.querySelector('#nav-bar')
 
 //Toggle the show class off aand on
